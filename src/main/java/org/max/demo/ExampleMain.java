2a1a7891e057b3ff839be5b479dfc3727a08f901
kravmaxim@gmail.com <Cegthgfhjkm!666>
2023-06-02 21:49:49 +0300

    -

--- src/main/java/org/max/demo/ExampleMain.java
+++
@@ -1,8 +0,0 @@
-package org.max.demo;
-
-public class ExampleMain {
-
-    public static void main(String[] args) {
-
-    }
-}
--- src/main/java/org/max/seminar/exception/DeckEmptyException.java
+++
@@ -1,8 +0,0 @@
-package org.max.seminar.exception;
-
-public class DeckEmptyException extends Exception{
-
-    public DeckEmptyException(String message) {
-        super(message);
-    }
-}
