ba66cac3b827506186b815b053f32fdda311f521
kravmaxim@gmail.com <Cegthgfhjkm!666>
2023-06-03 17:42:20 +0300

    -

--- src/test/java/org/max/demo/LogbackExampleTest.java
+++ src/test/java/org/max/demo/LogbackExampleTest.java
@@ -11,6 +11,21 @@ public class LogbackExampleTest {
 
     @Test
     void simpleExampleTest() {
+        logger.info("Example log from " + LogbackExampleTest.class.getSimpleName());
         logger.info("log String value to console");
+    }
+
+    @Test
+    void parametrizedMessageTest() {
+        String message = "This is a String";
+        Integer zero = 0;
+
+        try {
+            logger.debug("Logging message: {}", message);
+            logger.debug("Going to divide {} by {}", 42, zero);
+            int result = 42 / zero;
+        } catch (Exception e) {
+            logger.error("Error dividing {} by {} ", 42, zero, e);
+        }
     }
 }
