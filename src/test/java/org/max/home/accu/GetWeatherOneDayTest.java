995115ff165af2e3ac58c2df26cd963d302198fd
kravmaxim@gmail.com <Cegthgfhjkm!666>
2023-06-21 14:13:56 +0300

    -

---
+++ src/test/java/org/max/home/accu/GetWeatherOneDayTest.java
@@ -0,0 +1,10 @@
+package org.max.home.accu;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GetWeatherOneDayTest extends AbstractTest{
+
+    private static final Logger logger
+            = LoggerFactory.getLogger(GetWeatherOneDayTest.class);
+}
---
+++ src/test/java/org/max/seminar/accu/GetLocationTest.java
@@ -0,0 +1,10 @@
+package org.max.seminar.accu;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GetLocationTest extends AbstractTest{
+
+    private static final Logger logger
+            = LoggerFactory.getLogger(GetLocationTest.class);
+}
